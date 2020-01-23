ee1d9ddb965dcb8608d79209b1d479cf286a8b2e
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-24 01:16:34 +0600

    Create blog.js data

---
+++ data.js
@@ -0,0 +1,43 @@
+import BlogImageOne from './src/images/blog-1.jpg'
+import BlogImageTwo from './src/images/blog-2.jpg'
+import BlogImageThree from './src/images/blog-3.jpg'
+import BlogImageFour from './src/images/blog-4.jpg'
+import BlogImageFive from './src/images/blog-5.jpg'
+export const BlogList = [
+    {
+        id: 1,
+        img: BlogImageOne,
+        title: 'How to increase your ROI through scientific SEM?',
+        description: 'In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum, velit diam iaculis velit, in ornare massa enim at lorem.'
+    },
+    {
+        id: 2,
+        img: BlogImageTwo,
+        title: 'Planning your online business goals with a specialist',
+        description: 'In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum, velit diam iaculis velit, in ornare massa enim at lorem.'
+    },
+    {
+        id: 3,
+        img: BlogImageThree,
+        title: 'Getting ready for the new-age SEO revolution',
+        description: 'In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum, velit diam iaculis velit, in ornare massa enim at lorem.'
+    },
+    {
+        id: 4,
+        img: BlogImageFour,
+        title: 'The trends in Social Media Optimization & SEM',
+        description: 'In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum, velit diam iaculis velit, in ornare massa enim at lorem.'
+    },
+    {
+        id: 5,
+        img: BlogImageFive,
+        title: 'How to increase your ROI through scientific SEM?',
+        description: 'In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum, velit diam iaculis velit, in ornare massa enim at lorem.'
+    },
+    {
+        id: 6,
+        img: BlogImageOne,
+        title: 'Planning your online business goals with a specialist',
+        description: 'In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum, velit diam iaculis velit, in ornare massa enim at lorem.'
+    },
+]
