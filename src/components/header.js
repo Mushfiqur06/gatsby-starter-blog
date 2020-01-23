74558d6c58ae1d663cab5c3bde596b6eec7c2415
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-23 23:26:33 +0600

    Header component design

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,26 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
 
 import './../sass/main.scss'
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+  <header className="header">
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">{siteTitle}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   </header>
-)
+  )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
