916f499ba447dbf31dcbf42976e89a9dd274a57d
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-01-24 01:15:21 +0600

    blog Section Add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,51 @@
-import React from "react"
+import React, { Component } from 'react';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Blogs from './../components/Blogs'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    
-  </Layout>
-)
+import { Container, Row} from 'react-bootstrap'
+import { BlogList } from './../../data'
+
+class IndexPage extends Component{
+
+  state = {
+    blogs: []
+  }
+
+  componentDidMount(){
+    this.getBlogs()
+  }
+
+  getBlogs = () => {
+    let tempBlogs = []
+    BlogList.forEach(item => {
+      const singleitem = {...item}
+      tempBlogs = [...tempBlogs, singleitem]
+    })
+
+    this.setState({
+      blogs: tempBlogs
+    })
+  }
+
+  render(){
+
+    return (
+      <Layout>
+        <SEO title="Home" />
+
+        <section className="pt-5 pb-5 blogSection">
+          <Container>
+              
+            <Blogs blogs={this.state.blogs} />
+
+          </Container>
+        </section>
+      </Layout>
+    )
+
+  }
+}
 
 export default IndexPage
